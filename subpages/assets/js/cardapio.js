0c8086da0693d166c7d748feeeba8cd7300ffe2f
Caio Picciarelli <salapicc8@gmail.com>
2025-06-20 17:07:14 -0300

    atualização09 20/6/25 - Caio

--- subpages/assets/js/cardapio.js
+++ subpages/assets/js/cardapio.js
@@ -1,3 +1,5 @@
+//popup.js
+
 const btn = document.getElementById('btn-nav-account');
 const popup = document.getElementById('popup');
 
@@ -16,3 +18,56 @@ popup.addEventListener('click', (e) => {
 document.addEventListener('click', () => {
   popup.style.display = 'none';
 });
+
+//Modal destaques
+const btnOpenModalDestaques1 = document.getElementById('btn-add-destaques-1');
+const btnOpenModalDestaques2 = document.getElementById('btn-add-destaques-2');
+const btnOpenModalDestaques3 = document.getElementById('btn-add-destaques-3');
+const modalDestaques1 = document.getElementById('modal-d-1');
+const modalDestaques2 = document.getElementById('modal-d-2');
+const modalDestaques3 = document.getElementById('modal-d-3');
+
+
+//Abrir/fechar modal 1
+
+btnOpenModalDestaques1.addEventListener('click', () => {
+  modalDestaques1.style.display = 'flex';
+});
+
+modalDestaques1.addEventListener('click', (event) => {
+  // Se o clique for no próprio modal (fundo), fecha
+  if (event.target === modalDestaques1) {
+    modalDestaques1.style.display = 'none';
+  }
+});
+
+//Abrir/fechar modal 2
+
+btnOpenModalDestaques2.addEventListener('click', () => {
+  modalDestaques2.style.display = 'flex';
+});
+
+modalDestaques2.addEventListener('click', (event) => {
+  // Se o clique for no próprio modal (fundo), fecha
+  if (event.target === modalDestaques2) {
+    modalDestaques2.style.display = 'none';
+  }
+});
+
+//Abrir/fechar modal 3
+
+btnOpenModalDestaques3.addEventListener('click', () => {
+  modalDestaques3.style.display = 'flex';
+});
+
+modalDestaques3.addEventListener('click', (event) => {
+  // Se o clique for no próprio modal (fundo), fecha
+  if (event.target === modalDestaques3) {
+    modalDestaques3.style.display = 'none';
+  }
+});
+
+
+
+
+
