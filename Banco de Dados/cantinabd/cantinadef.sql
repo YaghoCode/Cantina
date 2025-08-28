99345bc9b7f976f45b951d0daf872f1706a743be
unknown <yaghochinaglia@gmail.com>
2025-08-27 22:12:35 -0300

    att bd

--- Banco de Dados/cantinabd/cantinadef.sql
+++ Banco de Dados/cantinabd/cantinadef.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 28/08/2025 às 02:01
+-- Tempo de geração: 28/08/2025 às 03:11
 -- Versão do servidor: 10.4.32-MariaDB
 -- Versão do PHP: 8.2.12
 
@@ -20,28 +20,6 @@ SET time_zone = "+00:00";
 --
 -- Banco de dados: `cantinadef`
 --
-
--- --------------------------------------------------------
-
---
--- Estrutura para tabela `cliente`
---
-
-CREATE TABLE `cliente` (
-  `nome` varchar(60) NOT NULL,
-  `cpf` char(11) NOT NULL,
-  `email` varchar(150) NOT NULL,
-  `turma` enum('1DS','2DS','3DS','') NOT NULL,
-  `senha` varchar(200) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Despejando dados para a tabela `cliente`
---
-
-INSERT INTO `cliente` (`nome`, `cpf`, `email`, `turma`, `senha`) VALUES
-('davi', '01424537437', 'gbidinoti@gmail.com', '2DS', '$2y$10$h181VJ/9evJZH9XzJXtgbeAn5hEV5ryK7EaTwLw5Dp74.uNtHau1G'),
-('yagho', '12345678910', 'yaghochinaglia@gmail.com', '1DS', '$2y$10$h/f2/zbIfQVINIbcX2nShOmq6hi0jNpAW46jM0djhpi9coBpDY5G2');
 
 -- --------------------------------------------------------
 
@@ -86,17 +64,15 @@ INSERT INTO `estoque` (`Nome`, `Descricao`, `Preco`, `Quantidade`, `Categoria`, 
 ('', '', 0, 0, '', 39, ''),
 ('Coxinha de Frango', 'Coxinha de frango gostoso', 1, 1, 'Doces', 40, ''),
 ('Coxinha de Frango', 'Coxinha de frango gostoso', 1, 1, 'Doces', 41, ''),
-('Esfiha de Frango', 'Coxinha de frango gostoso', 1, 1, 'Salgados', 42, '');
+('Esfiha de Frango', 'Coxinha de frango gostoso', 1, 1, 'Salgados', 42, ''),
+('Esfiha de Frango', 'Esfiha de frango gostoso', 1, 1, 'Salgados', 43, ''),
+('Esfiha de Frango', 'Esfiha de frango gostoso', 1, 1, 'Doces', 44, ''),
+('Esfiha de Frango', 'Esfiha de frango gostoso', 1, 1, 'Doces', 45, 'FolhadoPalmito.png'),
+('TESTEEEEEE', 'TESTEEEEEE', 100, 22, 'Folhados', 46, 'pudim.png');
 
 --
 -- Índices para tabelas despejadas
 --
-
---
--- Índices de tabela `cliente`
---
-ALTER TABLE `cliente`
-  ADD PRIMARY KEY (`cpf`);
 
 --
 -- Índices de tabela `estoque`
@@ -112,7 +88,7 @@ ALTER TABLE `estoque`
 -- AUTO_INCREMENT de tabela `estoque`
 --
 ALTER TABLE `estoque`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=43;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=47;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
