81e0c8363ba3da928a0994ae7c562fd70a01d3b8
CAIO PICCIARELLI SILVA <23373>
2025-08-28 11:28:29 -0300

    att

--- Banco de Dados/cantinabd/cantinadef.sql
+++ Banco de Dados/cantinabd/cantinadef.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 28/08/2025 às 03:11
+-- Tempo de geração: 28/08/2025 às 16:27
 -- Versão do servidor: 10.4.32-MariaDB
 -- Versão do PHP: 8.2.12
 
@@ -21,7 +21,11 @@ SET time_zone = "+00:00";
 -- Banco de dados: `cantinadef`
 --
 
+-- --------------------------------------------------------
 
+--
+-- Estrutura para tabela `cliente`
+--
 
 CREATE TABLE `cliente` (
   `nome` varchar(60) NOT NULL,
@@ -36,19 +40,62 @@ CREATE TABLE `cliente` (
 --
 
 INSERT INTO `cliente` (`nome`, `cpf`, `email`, `turma`, `senha`) VALUES
-('davi', '01424537437', 'gbidinoti@gmail.com', '2DS', '$2y$10$h181VJ/9evJZH9XzJXtgbeAn5hEV5ryK7EaTwLw5Dp74.uNtHau1G'),
-('yagho', '12345678910', 'yaghochinaglia@gmail.com', '1DS', '$2y$10$h/f2/zbIfQVINIbcX2nShOmq6hi0jNpAW46jM0djhpi9coBpDY5G2');
-
--- --------------------------------------------------------
-ALTER TABLE `cliente`
-  ADD PRIMARY KEY (`cpf`);
-
+('Lucas Nunes', '14356789741', 'lucasnunes23@gmail.com', '2DS', '$2y$10$Ah8phSo53QToDYmPVbx91ue//HDm6ai3dwv7AuhHSJUBjvGCJDkPK'),
+('111111', '44444444442', 'yaghochinaglia@gmail.com', '1DS', '$2y$10$CSXOfcR/FgNDyNWe1zl2P.LVAl217eSe/XFHD338c1NTAtkEN9SkS'),
+('111111', '44444444454', 'yaghochinaglia@gmail.com', '1DS', '$2y$10$LvYim08e/zSZAICmNunLieaC.96eozyusaKSiA1D8Clv1g1fA4JXi'),
+('111111', '44445555555', 'yaghochinaglia@gmail.com', '2DS', '$2y$10$mXmF6kywScxam1vL75Bm..HCZFnZC2ejGpj2B9ymyUxSolIVhgupu'),
+('111111', '44445555556', 'yaghochinaglia@gmail.com', '', '$2y$10$0din5MuSkPw8g8UjC9hQQuvDSjEfcA7LRB3BlxvAONKe63iLDNrlu');
 
 -- --------------------------------------------------------
 
 --
 -- Estrutura para tabela `estoque`
 --
+
+CREATE TABLE `estoque` (
+  `Nome` varchar(100) NOT NULL,
+  `Descricao` varchar(1000) NOT NULL,
+  `Preco` decimal(10,0) NOT NULL,
+  `Quantidade` int(11) NOT NULL,
+  `Categoria` enum('Salgados','Doces','Folhados','Bebidas','Outros') NOT NULL,
+  `id` int(11) NOT NULL,
+  `img` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Despejando dados para a tabela `estoque`
+--
+
+INSERT INTO `estoque` (`Nome`, `Descricao`, `Preco`, `Quantidade`, `Categoria`, `id`, `img`) VALUES
+('FRANGO DE CAPUTOITY KAKIA JUISJS', 'brigadeiro de chocolate aaaaaaaa aaaaaaaaa aaaaaaaaaaaa aaaaaaaaaaaa aaaaaaaaaaaa aaaaaaa', 7, 8, 'Salgados', 49, 'croissantcalabresa.png'),
+('FRANGO DE CAPUTOITY KAKIA JUISJS', 'brigadeiro de chocolate aaaaaaaaaaaaaaaa aaaaaaaaaaaa aaaaaaaaaaaa aaaaaaaaaaaa aaaaaaa', 7, 8, 'Salgados', 50, 'cha.png');
+
+--
+-- Índices para tabelas despejadas
+--
+
+--
+-- Índices de tabela `cliente`
+--
+ALTER TABLE `cliente`
+  ADD PRIMARY KEY (`cpf`);
+
+--
+-- Índices de tabela `estoque`
+--
+ALTER TABLE `estoque`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT para tabelas despejadas
+--
+
+--
+-- AUTO_INCREMENT de tabela `estoque`
+--
+ALTER TABLE `estoque`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=52;
+COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
