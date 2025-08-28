fc2aba286f1a833ddf06f4973d040428c188795f
unknown <yaghochinaglia@gmail.com>
2025-08-27 21:02:37 -0300

    bd upd

--- Banco de Dados/cantinabd/cantinadef.sql
+++ Banco de Dados/cantinabd/cantinadef.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 27/08/2025 às 16:41
+-- Tempo de geração: 28/08/2025 às 02:01
 -- Versão do servidor: 10.4.32-MariaDB
 -- Versão do PHP: 8.2.12
 
@@ -55,8 +55,38 @@ CREATE TABLE `estoque` (
   `Preco` decimal(10,0) NOT NULL,
   `Quantidade` int(11) NOT NULL,
   `Categoria` enum('Salgados','Doces','Folhados','Bebidas','Outros') NOT NULL,
-  `id` int(11) NOT NULL
+  `id` int(11) NOT NULL,
+  `img` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Despejando dados para a tabela `estoque`
+--
+
+INSERT INTO `estoque` (`Nome`, `Descricao`, `Preco`, `Quantidade`, `Categoria`, `id`, `img`) VALUES
+('Coxinha de Frango', 'Coxinha de frango gostoso', 10, 2, 'Salgados', 20, ''),
+('Esfiha de Frango', 'Esfiha de frango gostoso', 10, 3, 'Salgados', 21, ''),
+('Salgado Folhado', 'Salgado Folhado gostoso', 10, 2, 'Folhados', 22, ''),
+('Esfiha de Frango', 'Esfiha de Carne gostoso', 0, 5, 'Salgados', 23, ''),
+('Esfiha de Queijo', 'Esfiha de Queijo Gostoso', 6, 6, 'Salgados', 24, ''),
+('Esfiha de Queijo', 'Esfiha de Queijo Gostoso', 6, 6, 'Salgados', 25, ''),
+('Esfiha de Queijo', 'Esfiha de Queijo Gostoso', 6, 6, 'Salgados', 26, ''),
+('Coxinha de Frango', 'Esfiha de frango gostoso', 1, 1, 'Bebidas', 27, ''),
+('Coxinha de Frango', 'Esfiha de frango gostoso', 1, 1, 'Bebidas', 28, ''),
+('Coxinha de Frango', 'Esfiha de frango gostoso', 1, 1, 'Bebidas', 29, ''),
+('Coxinha de Frango', 'Esfiha de frango gostoso', 1, 1, 'Bebidas', 30, ''),
+('', '', 0, 0, '', 31, ''),
+('', '', 0, 0, '', 32, ''),
+('', '', 0, 0, '', 33, ''),
+('', '', 0, 0, '', 34, ''),
+('', '', 0, 0, '', 35, ''),
+('', '', 0, 0, '', 36, ''),
+('', '', 0, 0, '', 37, ''),
+('', '', 0, 0, '', 38, ''),
+('', '', 0, 0, '', 39, ''),
+('Coxinha de Frango', 'Coxinha de frango gostoso', 1, 1, 'Doces', 40, ''),
+('Coxinha de Frango', 'Coxinha de frango gostoso', 1, 1, 'Doces', 41, ''),
+('Esfiha de Frango', 'Coxinha de frango gostoso', 1, 1, 'Salgados', 42, '');
 
 --
 -- Índices para tabelas despejadas
@@ -82,7 +112,7 @@ ALTER TABLE `estoque`
 -- AUTO_INCREMENT de tabela `estoque`
 --
 ALTER TABLE `estoque`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=43;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
